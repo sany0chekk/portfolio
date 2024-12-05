877f7af884a59e90bdbfffea18a3a23103a530af
Oleksandr <olexandr.veseluy@gmail.com>
2024-12-05 17:37:28 +0200

    add new project

--- src/content/projects.ts
+++ src/content/projects.ts
@@ -1,6 +1,21 @@
 export const projects = [
   {
     id: 1,
+    name: "Elecar",
+    description:
+      "Elecar is a pet project designed to showcase and sell electric cars from leading brands. The website features a catalog of popular models with detailed specifications, search filters, and a subscription option for special offers. The project is built using modern technologies to ensure a seamless user experience and efficient data management.",
+    date: "1 week",
+    images: [
+      "/projects/elecar/img-desktop.png",
+      "/projects/elecar/img-tablet.png",
+      "/projects/elecar/img-mobile.png",
+    ],
+    skills: ["HTML", "CSS", "JavaScript"],
+    link: "https://sany0chekk.github.io/elecar/",
+    github: "https://github.com/sany0chekk/elecar",
+  },
+  {
+    id: 2,
     name: "Travel Trucks",
     description:
       "This website offers an intuitive interface for searching, viewing, and booking campervans. Users can effortlessly enter their personal information, select a preferred booking date, and leave additional comments or requests. The project utilizes the Formik library for efficient form state management and validation handling, while Yup is employed to define robust validation schemas ensuring data accuracy and integrity.",
@@ -15,7 +30,7 @@ export const projects = [
     github: "https://github.com/sany0chekk/travel-trucks",
   },
   {
-    id: 2,
+    id: 3,
     name: "Smartnotes",
     description:
       "This website example showcases my skills and expertise as a Frontend Developer. SmartNotes features a modern design, user-friendly interface, and seamless performance across various platforms. This example demonstrates my ability to develop functional and practical web applications.",
@@ -30,7 +45,7 @@ export const projects = [
     github: "https://github.com/sany0chekk/smartnotes",
   },
   {
-    id: 3,
+    id: 4,
     name: "Travlog",
     description:
       "Travlog is a web application for travel search, allowing users to find various trip options, view detailed information with descriptions and photos. The project is created to showcase front-end development skills.",
@@ -45,7 +60,7 @@ export const projects = [
     github: "https://github.com/sany0chekk/travlog",
   },
   {
-    id: 4,
+    id: 5,
     name: "Whitespace UI",
     description:
       "This website offers tools and resources for effectively scaling global teams. Visitors can explore methods to improve work and enhance employee happiness, save favorite pages, and access features on mobile devices. The site includes sections about the company, news, blog, and customer support. Key features include advanced search capabilities and a 14-day free trial.",
